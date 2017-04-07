fbe97584bffc7e71b0b387a0af4b85e6074a3836
MY RAILS <jhambrohan44@gmail.com>
2017-04-08 04:30:54 +0530

    added mailing settings and a little styling

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+ActionMailer::Base.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'allgodallgood12345@gmail.com',
+    password:             '12345allgodallgood',
+    authentication:       :plain,
+    enable_starttls_auto: true
+}
